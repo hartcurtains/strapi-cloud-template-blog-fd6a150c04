8f96b16c372bb759302a3f95cc1004256d756a68
digitaldoorway <digitaldoorway@users.noreply.github.com>
2026-08-10 16:24:26 +0100

    fix(auth): configure registration profile fields

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,4 +1,11 @@
 export default () => ({
+  'users-permissions': {
+    config: {
+      register: {
+        allowedFields: ['title', 'firstname', 'lastname'],
+      },
+    },
+  },
   'order-management': {
     enabled: true,
     resolve: './src/plugins/order-management',
